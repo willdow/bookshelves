96d9dfb3642d519039d0a39eede2502f3b44e3c1
Willyan LIN <willyanlin@gmail.com>
2020-04-14 00:47:23 +0200

    (feat) Add authentification with firebase + Header management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyDMaGJy6W3W2mnDQLUp6XhTZICPUnFHYB0",
+      authDomain: "bookshelves-91133.firebaseapp.com",
+      databaseURL: "https://bookshelves-91133.firebaseio.com",
+      projectId: "bookshelves-91133",
+      storageBucket: "bookshelves-91133.appspot.com",
+      messagingSenderId: "281386723627",
+      appId: "1:281386723627:web:bdc99576b25ce478c03d6e"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
---
+++ src/app/models/Book.model.ts
@@ -0,0 +1,7 @@
+export class Book {
+    photo: string;
+
+    constructor(public title: string, public author: string) {
+
+    }
+}
